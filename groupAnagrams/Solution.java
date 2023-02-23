3e14204b9e7f3384d178bb430aa4174f1d8cb025
Ibtisam Mirza <iuhmirza@gmail.com>
2023-02-23 15:22:57 +0000

    Improve runtime of group anagrams

--- groupAnagrams/Solution.java
+++ groupAnagrams/Solution.java
@@ -1,52 +1,33 @@
 package groupAnagrams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     public static List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> groups = new ArrayList<>();
-        List<String> firstGroup = new ArrayList<>();
-        firstGroup.add(strs[0]);
-        groups.add(firstGroup);
+        HashMap<String, List<String>> anagramMap = new HashMap<>();
 
-        for (int i = 1; i < strs.length; i++) {
-            String str = strs[i];
-            boolean inGroup = false;
-            for (List<String> group : groups) {
-                if (isAnagram(group.get(0), str)) {
-                    group.add(str);
-                    inGroup = true;
-                    break;
-                }
+        for (String str : strs) {
+            int[] charCount = new int[26];
+
+            int n = str.length();
+            for (int i = 0; i < n; i++) {
+                charCount[str.charAt(i) - 'a']++;
             }
-            if (!inGroup) {
-                List<String> newGroup = new ArrayList<>();
-                newGroup.add(str);
-                groups.add(newGroup);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0 ; i < 26; i ++) {
+                sb.append(charCount[i]);
+                sb.append(",");
             }
+
+            String repr = sb.toString();
+
+            if (!anagramMap.containsKey(repr)) {
+                anagramMap.put(repr, new ArrayList<>());
+            }
+            anagramMap.get(repr).add(str);
         }
 
-        return groups;
-    }
-
-    public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        int[] charCount = new int[26];
-
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            charCount[s.charAt(i) - 'a']++; // have reference char count for each group
-            charCount[t.charAt(i) - 'a']--;
-        }
-
-        for (int i : charCount) {
-            if (i != 0) return false;
-        }
-
-        return true;
+        return new ArrayList<>(anagramMap.values());
     }
 }
