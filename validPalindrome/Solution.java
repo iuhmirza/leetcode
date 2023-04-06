47214a3fb0d05f5c9840e7d67636b2afa5a74901
Ibtisam Mirza <iuhmirza@gmail.com>
2023-04-06 18:48:13 +0100

    Complete valid palindrome

--- validPalindrome/Solution.java
+++ validPalindrome/Solution.java
@@ -1,2 +1,27 @@
-package validPalindrome;public class Solution {
+package validPalindrome;
+
+public class Solution {
+    public static boolean isPalindrome(String s) {
+        s = s.toUpperCase();
+        int leftPointer = 0;
+        int rightPointer = s.length() - 1;
+        char leftChar;
+        char rightChar;
+        while (leftPointer < rightPointer) {
+            leftChar = s.charAt(leftPointer);
+            rightChar = s.charAt(rightPointer);
+            if (leftChar < 48 || (leftChar > 57 && leftChar < 65) || leftChar > 90) {
+                leftPointer++;
+                continue;
+            }
+            if (rightChar < 48 || (rightChar > 57 && rightChar < 65) || rightChar > 90) {
+                rightPointer--;
+                continue;
+            }
+            if (leftChar != rightChar) return false;
+            leftPointer++;
+            rightPointer--;
+        }
+        return true;
+    }
 }
